3bfc11ae5e48339d15f9c587ae1d8735158ecbd7
kkomo11 <djqpsdj@naver.com>
2023-01-07 01:33:27 +0900

    Header 생성

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import Header from "/src/components/common/Header.vue";
+export default {
+  name: "HelloWorld",
+  components: {
+    Header,
+  },
+};
 </script>
 
 <template>
+  <Header></Header>
   <div>
     <router-link to="/">Home</router-link>
     <router-view />
   </div>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
